7989203d89f3bf1dec8e96be7baebb4a66f1b5e2
Hugo FABRE <hugo.fabre@epitech.eu>
2016-06-30 01:33:53 +0200

    hotfix

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 To handle a new message juste write the following:
 
+- For a classic method
 ```javascript
 StreamingServices.prototype.MessageName = function (params) {
  // Do something here
-}
+};
+
+- For a static method
+```javascript
+StreamingServices.MessageName = function (params) {
+ // Do something here
+};
--- interactions-messages.js
+++ interactions-messages.js
@@ -2,16 +2,16 @@
  * Created by fabre_h on 6/30/16.
  */
 
-StreamingServices.prototype.drawInteraction = function (params) {
+StreamingServices.draw_interaction = function (params) {
     var interaction = params['interaction'];
-    params.canvasContext = canvasContext;
+    params.canvas_context = canvas_context;
     if (Interaction.is_valid(interaction)) {
         var i = new StreamingServices[interaction](params);
         i.show();
     }
 };
 
-StreamingServices.prototype.removeInteraction = function (params) {
+StreamingServices.remove_interaction = function (params) {
     var i = Interaction.find_by_id(params['interaction_id']);
     i.remove();
 };
