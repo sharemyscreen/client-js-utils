f5871772fee207a30fc211a35b6f81d60296068c
Hugo FABRE <hugo.fabre@epitech.eu>
2016-07-03 18:11:34 +0200

    hotfix

--- interactions-messages.js
+++ interactions-messages.js
@@ -2,16 +2,16 @@
  * Created by fabre_h on 6/30/16.
  */
 
-StreamingServices.draw_interaction = function (params) {
-    var interaction = params['interaction'];
-    params.canvas_context = canvas_context;
-    if (StreamingServices[interaction] !== undefined) {
-        var i = new StreamingServices[interaction](params);
-        i.show();
-    }
-};
-
-StreamingServices.remove_interaction = function (params) {
-    var i = Interaction.find_by_id(params['interaction_id']);
-    i.remove();
-};
+//StreamingServices.draw_interaction = function (params) {
+//    var interaction = params['interaction'];
+//    params.canvas_context = canvas_context;
+//    if (StreamingServices[interaction] !== undefined) {
+//        var i = new StreamingServices[interaction](params);
+//        i.show();
+//    }
+//};
+//
+//StreamingServices.remove_interaction = function (params) {
+//    var i = Interaction.find_by_id(params['interaction_id']);
+//    i.remove();
+//};
